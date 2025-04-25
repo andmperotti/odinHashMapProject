b977919211b202acf037ec408bed7bba81afbab4
Andrew Perotti <Perotti28@gmail.com>
2025-04-25 11:07:05 -0700

    Change capacity default value, add hash method
    Capacity represents the number of buckets or positions to store data in in our hashmap, which I'll start at a value of 16.
    The hash method is the method that calculates a hash given a key value, and the value it returns will be used as the index in the hashmap to reach one of these buckets of data. I used a hash function that was defined by the lesson and added the action of dividing the calculation by the capacity value. This capacity value can change later when more space is needed, and when that is required then all keys will be revaluated to reposition data.

--- hash_map.js
+++ hash_map.js
@@ -1,6 +1,18 @@
 export default class HashMap {
-  constructor(loadFactor, capacity) {
+  constructor(loadFactor, capacity = 16) {
     this.loadFactor = loadFactor;
     this.capacity = capacity;
+  }
+
+  //using the provided hashing function from the lesson, but added a modulos step so we create 16 'buckets' for data in our hash map
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
   }
 }
