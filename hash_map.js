3db7aeea56a2ca452b15e7387e7d7df1139b3669
Andrew Perotti <Perotti28@gmail.com>
2025-04-25 13:14:15 -0700

    Add hash method, buckets variable to class
    The hash method was provided by the lesson and I'd rather not go down a rabbit hole so I'll use their hash function.
    I also created a buckets variable which is the array which is representing the hash table.

--- hash_map.js
+++ hash_map.js
@@ -1,3 +1,16 @@
 export default function HashMap(loadFactor = 0.75, capacity = 16) {
-  return {};
+  let buckets = Array.from({length: capacity}, (e) => null);
+
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
+  return {hash};
 }
