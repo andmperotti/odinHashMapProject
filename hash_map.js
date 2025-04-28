48a3725990112f7bf81c1917a003515de1f3f6e7
Andrew Perotti <Perotti28@gmail.com>
2025-04-28 09:18:51 -0700

    Add method requirement comments, add get method
    I've gone ahead and added the instructions/requirements for each method above the method declaration, this way I reduce the number of times I have to look at the project assignment page.
    I've added the get method, which when the user gives a key we return the value stored via hashing that key.

--- hash_map.js
+++ hash_map.js
@@ -4,6 +4,7 @@ import Node from "./node_instance.js";
 export default function HashMap(loadFactor = 0.75, capacity = 16) {
   let buckets = Array.from({length: capacity}, (e) => null);
 
+  //"takes a key and produces a hash code with it. We already implemented a fairly good hash function in the previous lesson"... I've used that one here
   function hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
@@ -14,6 +15,7 @@ export default function HashMap(loadFactor = 0.75, capacity = 16) {
     return hashCode;
   }
 
+  //takes two arguments: the first is a key, and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten, and we can say that we update the key’s value (e.g. Carlos is our key but it is called twice: once with value I am the old value., and once with value I am the new value.. Following this logic, Carlos should contain only the latter value).
   function set(key, value) {
     let index = hash(key);
     //if index is > buckets.length then there has been some kind of an error and you should return an error
@@ -27,8 +29,25 @@ export default function HashMap(loadFactor = 0.75, capacity = 16) {
       //otherwise there is a linked list at that bucket and we want to just add a node to the end of the linked list
       buckets[index].append(value);
     }
+    //"it’s important to grow buckets exactly as they are being expanded"
   }
-  //it’s important to grow buckets exactly as they are being expanded.
+
+  //takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+  //wouldn't that just give the first nodes value at that bucket?
+  function get(key) {
+    let index = hash(key);
+    if (index < 0 || index >= buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    } else if (buckets.index) {
+      //if value is found
+      return buckets.index;
+    } else {
+      //if value isn't found
+      return null;
+    }
+  }
 
   return {set};
 }
+
+//going to have to check load factor each time something is added
