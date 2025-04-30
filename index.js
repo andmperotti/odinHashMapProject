4fccb2d67e1b2c07938be1b4912469ad77a46877
Andrew Perotti <Perotti28@gmail.com>
2025-04-30 10:11:52 -0700

    Add testing invocations
    I added further method invocations for testing purposes as I had changed quite a bit since the last testing session.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 import HashMap from "./hash_map.js";
+
+const test = new HashMap(); // or HashMap() if using a factory
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+//add more entries so the loadFactor is passed, which causes doubling to occur to the array size
+test.set("sweatshirt", "black");
+test.set("glasses", "brown");
+test.set("skin", "tan"); //lie
+test.set("bottle", "green");
+test.set("sweatpants", "black");
+console.log(test.length());
+console.log(test.entries());
+// test.clear();
+// console.log(test.length()); //0
+test.set("ice cream", "green");
+console.log(test.get("ice cream")); //green
+console.log(test.has("apple"));
+console.log(test.remove("apple"));
+console.log(test.has("apple"));
+test.clear();
+console.log(test.length());
